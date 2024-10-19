442e198d9c96ab6ca3e280c12b96bf67bc8e37c9
Sakshi Powar <153968412+sakshipowar1612@users.noreply.github.com>
2024-10-19 12:50:41 +0530

    Delete loop directory

--- loop/factorial.py
+++
@@ -1,8 +0,0 @@
-def fact(num):
-    ans = 1
-    for i in range(1,num+1):
-        ans=ans*i
-    return ans
-
-num = int(input("Enter a number: "))
-print(fact(num))
--- loop/fibonachi.py
+++
@@ -1,9 +0,0 @@
-n = int(input('Enter number of teerms: '))
-a = 0
-b = 1
-
-for i in range(0,n):
-    print(a)
-    c =a + b
-    a = b
-    b = c
